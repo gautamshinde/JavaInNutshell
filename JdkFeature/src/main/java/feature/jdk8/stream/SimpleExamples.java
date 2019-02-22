ad069de73ff07a5c81ec0a6b142429305ce6231a
Shinde, Gautam <Gautam.Shinde@naunce.com>
2019-02-22 18:17:07 +0530

    Simple map fiter sample added

--- JdkFeature/src/main/java/feature/jdk8/stream/SimpleExamples.java
+++ JdkFeature/src/main/java/feature/jdk8/stream/SimpleExamples.java
@@ -12,12 +12,10 @@ public class SimpleExamples {
 	public static void sample1() {
 		List<String> list = List.of("Java", "GoTo", "Scala","Python","Kotlin");	
 		list.stream().forEach(System.out::println);
-		
 		//JDK 11
 		String language = "GOLang";		
 		List<String> list1  = language.codePoints().mapToObj(Character::toString).collect(toList());
 		System.out.println(list1);
-		
 		//Map & Filter
 		list.stream().map(String::toUpperCase).filter(s -> s.length() == 6).forEach(System.out::println);
 		
