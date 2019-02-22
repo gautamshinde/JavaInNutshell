7b5e864b27a6931967e1fae0dcabdca947fe1b5b
Shinde, Gautam <Gautam.Shinde@naunce.com>
2019-02-22 18:16:40 +0530

    Stream sample added

---
+++ JdkFeature/src/main/java/feature/jdk8/lambda/ComparatorExample.java
@@ -0,0 +1,93 @@
+package feature.jdk8.lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+import feature.jdk8.model.Person;
+
+public class ComparatorExample {
+	public static void main(String[] args) {
+		sample1();
+		sample2();
+		sample3();
+	}
+
+	public static List<Person> initialize() {
+		List<Person> list = new ArrayList<>();
+		list.add(new Person("Viman", "Nagar", 37));
+		list.add(new Person("Elphinston", "Kirkee", 30));
+		list.add(new Person("Kirkee", "Station", 4));
+		list.add(new Person("Lohgad", "Fort", 24));
+		list.add(new Person("Murud", "Fort", 26));
+		return list;
+	}
+
+	public static void sample1() {
+		List<Person> list = initialize();
+		Collections.sort(list, new Comparator<Person>() {
+			@Override
+			public int compare(Person p1, Person p2) {
+				int comp = p1.getLastname().compareTo(p2.getLastname());
+				if (comp == 0) {
+					comp = p1.getFirstname().compareTo(p2.getFirstname());
+					if (comp == 0) {
+						return Integer.compare(p1.getAge(), p2.getAge());
+					} else {
+						return comp;
+					}
+				} else {
+					return comp;
+				}
+			}
+		});
+		System.out.println("Sorted List " + list);
+	}
+
+	public static void sample2() {
+		Comparator<Person> cmp = Comparator.comparing(Person::getLastname).thenComparing(Person::getFirstname)
+				.thenComparing(Person::getAge);
+		List<Person> list = initialize();
+		Collections.sort(list, cmp);
+		System.out.println("Sorted List " + list);
+	}
+
+	public static void sample3() {
+		System.out.println("***** Sample3 *****");
+		List<Person> list = initialize();
+		Function<Person, String> getLastname = s -> s.getLastname();
+		// Function<Person, String> getFirstname = s -> s.getFirstname();
+		Comparator1<Person> c = Comparator1.comparing(getLastname).thenComparing((Person p) -> p.getLastname());
+		System.out.println("Compare = " + c.compare(list.get(0), list.get(1)));
+	}
+}
+
+@FunctionalInterface
+interface Comparator1<T> {
+	int compare(T t1, T t2);
+
+	static <T, U extends Comparable<? super U>> Comparator1<T> comparing(Function<T, U> keyExtr) {
+		Objects.requireNonNull(keyExtr);
+		return (p1, p2) -> {
+			U u1 = keyExtr.apply(p1);
+			U u2 = keyExtr.apply(p2);
+			return u1.compareTo(u2);
+		};
+	}
+
+	default <U extends Comparable<? super U>> Comparator1<T> thenComparing(Function<T, U> keyExtr) {
+		Objects.requireNonNull(keyExtr);
+		return (p1, p2) -> {
+			int cmp = this.compare(p1, p2);
+			if (cmp == 0) {
+				Comparator1<T> other = comparing(keyExtr);
+				return other.compare(p1, p2);
+			} else {
+				return cmp;
+			}
+		};
+	}
+}
---
+++ JdkFeature/src/main/java/feature/jdk8/stream/SimpleExamples.java
@@ -0,0 +1,29 @@
+package feature.jdk8.stream;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+public class SimpleExamples {
+	public static void main(String[] args) {
+		sample1();
+	}
+	
+	public static void sample1() {
+		List<String> list = List.of("Java", "GoTo", "Scala","Python","Kotlin");	
+		list.stream().forEach(System.out::println);
+		
+		//JDK 11
+		String language = "GOLang";		
+		List<String> list1  = language.codePoints().mapToObj(Character::toString).collect(toList());
+		System.out.println(list1);
+		
+		//Map & Filter
+		list.stream().map(String::toUpperCase).filter(s -> s.length() == 6).forEach(System.out::println);
+		
+	}
+	
+	public static void sample2() {
+		
+	}
+}
